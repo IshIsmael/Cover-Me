1d55e29df45696348444b7ab8c88555b372b3dc4
IshIsmael <150946594+IshIsmael@users.noreply.github.com>
2025-10-06 12:36:20 +0100

    redirect

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page - redirect to login */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // If user is already logged in, redirect to their dashboard
+  if (req.isAuthenticated()) {
+    if (req.user.role === 'admin') {
+      return res.redirect('/admin/dashboard');
+    } else if (req.user.role === 'instructor') {
+      return res.redirect('/instructor/timetable');
+    }
+  }
+  
+  // Otherwise redirect to login
+  res.redirect('/auth/login');
 });
 
 module.exports = router;
